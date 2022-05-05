342c64c2f94fd5ab593e347894f6aa7376062015
Mathias Hove <mathiashove02@gmail.com>
2022-05-05 16:22:48 +0200

    formatting

--- src/Enterspeed.Umbraco.Migrator/Enterspeed/SchemaImporter.cs
+++ src/Enterspeed.Umbraco.Migrator/Enterspeed/SchemaImporter.cs
@@ -63,23 +63,27 @@ namespace Enterspeed.Umbraco.Migrator.Enterspeed
             return documentTypes;
         }
 
-        private DocumentTypeBaseProperties GetPageBaseProperties(DeliveryApiResponse response, DocumentTypes documentTypes)
+        private DocumentTypeBaseProperties GetPageBaseProperties(DeliveryApiResponse response,
+            DocumentTypes documentTypes)
         {
             if (!response.Response.Route.TryGetValue("pageDocType", out object pageDocType))
             {
-                throw new NullReferenceException("pageDocType not found on schema for " + JsonSerializer.Serialize(pageDocType));
+                throw new NullReferenceException("pageDocType not found on schema for " +
+                                                 JsonSerializer.Serialize(pageDocType));
             }
 
             if (pageDocType is not Dictionary<string, object> pageDocTypeDict ||
                 !pageDocTypeDict.TryGetValue("view", out object view))
             {
-                throw new NullReferenceException("pageDocType view property not found on schema " + JsonSerializer.Serialize(pageDocType));
+                throw new NullReferenceException("pageDocType view property not found on schema " +
+                                                 JsonSerializer.Serialize(pageDocType));
             }
 
             if (view is not Dictionary<string, object> viewDict ||
                 !viewDict.TryGetValue("documentType", out object docType))
             {
-                throw new NullReferenceException("document type property not found on schema " + JsonSerializer.Serialize(pageDocType));
+                throw new NullReferenceException("document type property not found on schema " +
+                                                 JsonSerializer.Serialize(pageDocType));
             }
 
 
@@ -87,7 +91,8 @@ namespace Enterspeed.Umbraco.Migrator.Enterspeed
                 !docTypeDict.TryGetValue("alias", out object alias) ||
                 !docTypeDict.TryGetValue("name", out object name))
             {
-                throw new NullReferenceException("alias or name property not found on schema " + JsonSerializer.Serialize(pageDocType));
+                throw new NullReferenceException("alias or name property not found on schema " +
+                                                 JsonSerializer.Serialize(pageDocType));
             }
 
             return new DocumentTypeBaseProperties
@@ -100,7 +105,7 @@ namespace Enterspeed.Umbraco.Migrator.Enterspeed
         private void AddUrl(Child child, List<string> urls)
         {
             urls.Add(child.View.Self.View.Url);
-            if (child.View.Children.Any())
+            if (child.View.Children != null && child.View.Children.Any())
             {
                 foreach (var subChild in child.View.Children)
                 {
